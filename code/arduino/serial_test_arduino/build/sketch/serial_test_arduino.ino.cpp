0170b25542de0fd7d7fc29d8e4208c4d8f83e2b5
XiXinping <about@example.com>
2021-12-21 18:58:47 +0000

    Added ability to send commands to the arduino and have it respond in serial

--- code/arduino/serial_test_arduino/build/sketch/serial_test_arduino.ino.cpp
+++ code/arduino/serial_test_arduino/build/sketch/serial_test_arduino.ino.cpp
@@ -25,28 +25,13 @@ String line = "";
 void loop() {                                      
     if (Serial.available() > 0) {                  
         line = Serial.readStringUntil('\n');
-        int command = line.toInt();
-        if(command == 0) {
+        if(line == "0") {
             flash_led();
         }
-        if(command == 1) {
+        if(line == "1") {
             Serial.println("Echo!");
         }
         line = "";
     }
 }
-// void loop() {
-    //char receive_data = Serial.read();
-    //if(receive_data == 0) {
-        //Serial.println("echo");
-    //}
-    //switch(receive_data) {
-        //case 1:
-            //Serial.println("receive_data");
-        //case 2:
-            //flash_led();
-        //default:
-           //Serial.println("aidfasdufh");
-    //}
-//}
 
