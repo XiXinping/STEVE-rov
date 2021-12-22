c655e564bb0617e19325f1f3caf2fa4b738f7954
XiXinping <about@example.com>
2021-12-22 19:23:42 +0000

    example code for deserializing JSON strings on the Arduino

--- code/arduino/json_test_arduino/json_test_arduino.ino
+++ code/arduino/json_test_arduino/json_test_arduino.ino
@@ -4,13 +4,13 @@ void setup() {
     Serial.begin(9600);
     
     // a simple json string, remember to escape quotation marks
-    const char* input = "{\"command\": \"echo\", \"coordinates\": [0.99, 0.42]}";
+    const char* input = "{\"command\": \"echo\", \"coordinates\": [0.99, 0.42]}"
     
     // creates a json document on the stack
-    StaticJsonDocument<64> document;
+    StaticJsonDocument<32> doc;
     
     // deserializeJson() doesn't throw errors, rather they can be collected in this variable of the the type DeserializationError
-    DeserializationError err = deserializeJson(document, input);
+    DeserializationError err = deserializeJson(doc, input);
     if(err) {
         Serial.print("Error: ");
         Serial.println(err.c_str());
@@ -18,14 +18,12 @@ void setup() {
     }
     // parses data from the json document and stores them as variables
     const char* command = document["command"];
-    float x_coord = document["coordinates"][0];  // you can extract nested values
-    float y_coord = document["coordinates"][1];
+    float x_coord = document["joystick_coords"][0];  // you can extract nested values
+    float y_coord = document["joystick_coords"][1];
 
     Serial.println(command);
     Serial.println(x_coord);
     Serial.println(y_coord);
-
-    Serial.end();
 }
 
 void loop() {
