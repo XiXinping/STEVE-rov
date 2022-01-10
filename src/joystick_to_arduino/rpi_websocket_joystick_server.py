ddb0f764c13eb4b1cd2fc616eabdb1e60e63b71e
XiXinping <about@example.com>
2022-01-10 19:20:36 +0000

    Added ability to send commands to the arduino using strings as commands

--- src/joystick_to_arduino/rpi_websocket_joystick_server.py
+++ src/joystick_to_arduino/rpi_websocket_joystick_server.py
@@ -8,20 +8,20 @@ ser.reset_input_buffer()
 
 async def receive_data(websocket, path):
     # receives data from websocket client
-    async for joystick_data in websocket:
+    async for message in websocket:
+        joystick_data = json.loads(message)
+        arduino_command = None
         if joystick_data["button_values"][0] == 1:
-            arduino_command = {"command": "flash", "data": 8}
-        elif joystick_data["button_values"][1] == 2:
-            arduino_command = {"command": "flash", "data": 9}
-        
-        ser.write(arduino_command.encode('ascii'))
-
-        receive_line = ser.readline()
-        print("Arduino Response: " + receive_line.decode('ascii'))
+            arduino_command = {"command": "flash_off", "data": 8}
+        elif joystick_data["button_values"][1] == 1:
+            arduino_command = {"command": "flash_on", "data": 9}
+        if arduino_command:
+            arduino_command_send = json.dumps(arduino_command) + "\n"
+            ser.write(arduino_command_send.encode('ascii'))
 
 async def main():
     print('Server Started!')
-    async with websockets.serve(receive_data, "0.0.0.0", 1234):
+    async with websockets.serve(receive_data, "0.0.0.0", 8765):
         await asyncio.Future()  # run forever
 
 
