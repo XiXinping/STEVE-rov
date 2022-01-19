1ac84695c573ec19fe5eaf43cad53b336c990ca5
XiXinping <about@example.com>
2022-01-19 03:08:15 +0000

    Main server stuff

--- src/main_server/main_server.py
+++ src/main_server/main_server.py
@@ -7,23 +7,39 @@ ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
 
-async def receive_data(websocket, path):
-    # receives data from websocket client
-    async for message in websocket:
-        joystick_data = json.loads(message)
-        arduino_command = None
-        if joystick_data["button_values"][0] == 1:
-            arduino_command = {"command": "flash_off", "data": 8}
-        elif joystick_data["button_values"][1] == 1:
-            arduino_command = {"command": "flash_on", "data": 9}
-        if arduino_command:
-            arduino_command_send = json.dumps(arduino_command) + "\n"
-            ser.write(arduino_command_send.encode('ascii'))
+class WebsocketServer:
+    ws_data = None
+
+    def pump_ws_data(ws_data):
+        return ws_data
+
+    async def handler(websocket, path):
+        # receives data from websocket client
+        async for message in websocket:
+            ws_data = json.loads(message)
+
+
+async def main_server():
+    while True:
+        joystick_data = WebsocketServer.ws_data
+        print(joystick_data)
+        asyncio.sleep(0.1)
+
+
+def arduino_stuff(joystick_data):
+    arduino_command = None
+    if joystick_data["button_values"][0] == 1:
+        arduino_command = {"command": "flash_off", "data": 8}
+    elif joystick_data["button_values"][1] == 1:
+        arduino_command = {"command": "flash_on", "data": 9}
+    if arduino_command:
+        arduino_command_send = json.dumps(arduino_command) + "\n"
+        ser.write(arduino_command_send.encode('ascii'))
 
 
 async def main():
     print('Server Started!')
-    async with websockets.serve(receive_data, "0.0.0.0", 8765):
+    async with websockets.serve(WebsocketServer.handler, "0.0.0.0", 8765):
         await asyncio.Future()  # run forever
 
 
