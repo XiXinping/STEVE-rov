fbc52a5774571dfadab3566f004a7c723f14ef49
XiXinping <about@example.com>
2022-02-16 19:21:21 +0000

    Motor test files

--- src/motors/motor_test/build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/pi/underwater-rov/src/motors/motor_test/build/core/WString.cpp.o: \
- /home/pi/.arduino15/packages/arduino/hardware/avr/1.8.4/cores/arduino/WString.cpp \
- /home/pi/.arduino15/packages/arduino/hardware/avr/1.8.4/cores/arduino/WString.h
