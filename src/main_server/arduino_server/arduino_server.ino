6c9061effba3cf7775ca9c591b2f60939b10f99c
Random Thinkpad <about@example.com>
2022-04-20 14:24:59 -0400

    Arduino server file (WIP) and web client tweaks)

--- src/main_server/arduino_server/arduino_server.ino
+++ src/main_server/arduino_server/arduino_server.ino
@@ -2,53 +2,24 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <ArduinoJson.h>
 #include <Adafruit_LiquidCrystal.h>
+
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 Adafruit_LiquidCrystal lcd(0);
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
+
 #define SERVOMIN 150 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 600 // This is the 'maximum' pulse length count (out of 4096)
 #define USMIN 1100 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX 1900 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define MIN_SPEED 127 // minimum motor speed
+#define MAX_SPEED -127 // maximum motor speed
 
-////////////////////////////////////////////////////////////////////////////////////
-int8_t minIn = -127;  //the minimum input value for the motor speed
-int8_t maxIn = 127; //the maximum input value for the motor speed
-////////////////////////////////////////////////////////////////////////////////////
-
-
-// You can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
-
-// not being used
-/*void setServoPulse(uint8_t n, double pulse) {*/
-    /*double pulselength;*/
-  
-    /*pulselength = 1000000;   // 1,000,000 us (microseconds) per second*/
-    /*pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates*/
-    /*Serial.print(pulselength);*/
-    /*Serial.println(" us per period"); */
-    /*pulselength /= 4096;  // 12 bits of resolution*/
-    /*Serial.print(pulselength);*/
-    /*Serial.println(" us per bit"); */
-    /*pulse *= 1000000;  // convert input seconds to us*/
-    /*pulse /= pulselength;*/
-    /*Serial.println(pulse);*/
-    /*pwm.setPWM(n, 0, pulse);*/
-/*}*/
 
 void fire_motor(int motor_num, int velocity) {
     // convert the velocity into a microsecond delay that the motor can use
-    int microsecond_delay = map(velocity, -127, 127, USMIN, USMAX);
+    int microsecond_delay = map(velocity, -127, 127, MIN_SPEED, MAX_SPEED);
     pwm.writeMicroseconds(motor_num, microsecond_delay);
 }
 
@@ -140,42 +111,24 @@ int z_velocity = 0;
 int yaw_velocity = 0;
 
 void setup() {
-     Serial.begin(115200);
+    Serial.begin(115200);
 
-     pwm.begin();
-     lcd.begin(16, 2);
-     /*
-     * In theory the internal oscillator (clock) is 25MHz but it really isn't
-     * that precise. You can 'calibrate' this by tweaking this number until
-     * you get the PWM update frequency you're expecting!
-     * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-     * is used for calculating things like writeMicroseconds()
-     * Analog servos run at ~50 Hz updates, It is importaint to use an
-     * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-     * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-     *    the I2C PCA9685 chip you are setting the value for.
-     * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-     *    expected value (50Hz for most ESCs)
-     * Setting the value here is specific to each individual I2C PCA9685 chip and
-     * affects the calculations for the PWM update frequency. 
-     * Failure to correctly set the int.osc value will cause unexpected PWM results
-     */
+    pwm.begin();
+    lcd.begin(16, 2);
+
     pwm.setOscillatorFrequency(27000000);
     pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
-    stop_all();
+    stop_all();  // make sure all motors are stopped before beginning
 
     lcd.print("S.T.E.V.E  ROV");
     lcd.setCursor(0, 1);
-    lcd.print("Version 0.1.1");
+    lcd.print("Version 0.1.0");
     delay(1000);
     lcd.clear();
 
     // countdown
     for(int i = 3; i > 0; i--) {
-        Serial.print("Starting in ");
-        Serial.print(i);
-        Serial.println(" second(s)!");
         lcd.setCursor(1, 0);
         lcd.print("Starting in ");
         lcd.print(i);
@@ -186,17 +139,10 @@ void setup() {
     }
     lcd.clear();
 
+    // test each motor individually
     for(int i = 0; i < 6; i++) {
         lcd.print("Testing Motor ");
         lcd.print(i);
-        if (i == 9) {
-            lcd.setCursor(1, 1);
-            delay(500);
-            lcd.print("Just kidding!");
-            delay(500);
-            lcd.clear();
-            continue;
-        }
         fire_motor(i, 20);
         delay(1000);
         lcd.clear();
@@ -215,17 +161,8 @@ void loop() {
         // creates a json document on the stack                                     
         StaticJsonDocument<256> doc;                                            
  
-        // deserializeJson() doesn't throw errors, rather they can be collected in this variable of the the type DeserializationError
         DeserializationError err = deserializeJson(doc, receive_joystick_data);                
-        if(err) {                                                                   
-            Serial.print("Error: ");                                                
-            Serial.println(err.c_str());
-            /*lcd.setCursor(1, 0);*/
-            /*lcd.print("Error: ");*/
-            /*lcd.setCursor(1, 1);*/
-            /*lcd.print(err.c_str());*/
-            return;                                                                 
-        }                                                                           
+
         // parses data from the json document and stores them as variables          
         x_velocity = doc["x"];
         y_velocity = doc["y"];
--- src/motors/motor_test/motor_test.ino
+++ src/motors/motor_test/motor_test.ino
@@ -1,22 +1,3 @@
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 8 servos, one after the other on the
-  first 8 pins of the PCA9685
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-  
-  These drivers use I2C to communicate, 2 pins are required to  
-  interface.
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <ArduinoJson.h>
@@ -44,25 +25,26 @@ int8_t minIn = -127;  //the minimum input value for the motor speed
 int8_t maxIn = 127; //the maximum input value for the motor speed
 ////////////////////////////////////////////////////////////////////////////////////
 
-// our servo # counter
-uint8_t servonum = 0;
-
 
 // You can use this function if you'd like to set the pulse length in seconds
 // e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
-void setServoPulse(uint8_t n, double pulse) {
-    double pulselength;
+
+// not being used
+/*void setServoPulse(uint8_t n, double pulse) {*/
+    /*double pulselength;*/
   
-    pulselength = 1000000;   // 1,000,000 us (microseconds) per second
-    pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-    Serial.print(pulselength); Serial.println(" us per period"); 
-    pulselength /= 4096;  // 12 bits of resolution
-    Serial.print(pulselength); Serial.println(" us per bit"); 
-    pulse *= 1000000;  // convert input seconds to us
-    pulse /= pulselength;
-    Serial.println(pulse);
-    pwm.setPWM(n, 0, pulse);
-}
+    /*pulselength = 1000000;   // 1,000,000 us (microseconds) per second*/
+    /*pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates*/
+    /*Serial.print(pulselength);*/
+    /*Serial.println(" us per period"); */
+    /*pulselength /= 4096;  // 12 bits of resolution*/
+    /*Serial.print(pulselength);*/
+    /*Serial.println(" us per bit"); */
+    /*pulse *= 1000000;  // convert input seconds to us*/
+    /*pulse /= pulselength;*/
+    /*Serial.println(pulse);*/
+    /*pwm.setPWM(n, 0, pulse);*/
+/*}*/
 
 void fire_motor(int motor_num, int velocity) {
     // convert the velocity into a microsecond delay that the motor can use
@@ -108,20 +90,46 @@ void yaw(int8_t velocity, int8_t* motor_velocities) {
     motor_velocities[3] += velocity;
 }
 
-void fire_motors(int8_t x_velocity, int8_t y_velocity, int8_t z_velocity,
+void drive_motors(int8_t x_velocity, int8_t y_velocity, int8_t z_velocity,
         int8_t yaw_velocity) {
     int8_t motor_velocities[] = {0, 0, 0, 0, 0, 0};
     move_x(x_velocity, motor_velocities);
     move_y(y_velocity, motor_velocities);
     move_z(z_velocity, motor_velocities);
+    yaw(yaw_velocity, motor_velocities);
     for (uint8_t i; i < 6; i++) {
         fire_motor(i, motor_velocities[i]);
     }
 }
 void stop_all() {
-    for(int i = 0; i <= 5; i++) {
+    for(int i = 0; i < 6; i++) {
         fire_motor(i, 0);
     }
+}
+
+void print_velocities(int x_velocity, int y_velocity, int z_velocity,
+        int yaw_velocity) {
+    lcd.setCursor(0, 0);
+    lcd.print("X:    ");
+    lcd.setCursor(3, 0);
+    lcd.print(x_velocity);
+
+    lcd.setCursor(8, 0);
+    lcd.print("Y:    ");
+    lcd.setCursor(12, 0);
+    lcd.print(y_velocity);
+    lcd.print("    ");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Z:    ");
+    lcd.setCursor(3, 1);
+    lcd.print(z_velocity);
+
+    lcd.setCursor(8, 1);
+    lcd.print("Yaw:    ");
+    lcd.setCursor(12, 1);
+    lcd.print(yaw_velocity);
+    lcd.print("    ");
 }
 
 String receive_joystick_data = "";
@@ -132,31 +140,24 @@ int z_velocity = 0;
 int yaw_velocity = 0;
 
 void setup() {
-     Serial.begin(115200);
+    Serial.begin(115200);
 
-     pwm.begin();
-     lcd.begin(16, 2);
-     /*
-     * In theory the internal oscillator (clock) is 25MHz but it really isn't
-     * that precise. You can 'calibrate' this by tweaking this number until
-     * you get the PWM update frequency you're expecting!
-     * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-     * is used for calculating things like writeMicroseconds()
-     * Analog servos run at ~50 Hz updates, It is importaint to use an
-     * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-     * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-     *    the I2C PCA9685 chip you are setting the value for.
-     * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-     *    expected value (50Hz for most ESCs)
-     * Setting the value here is specific to each individual I2C PCA9685 chip and
-     * affects the calculations for the PWM update frequency. 
-     * Failure to correctly set the int.osc value will cause unexpected PWM results
-     */
+    pwm.begin();
+    lcd.begin(16, 2);
+
     pwm.setOscillatorFrequency(27000000);
     pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
     stop_all();
-    for(int i = 5; i > 0; i--) {
+
+    lcd.print("S.T.E.V.E  ROV");
+    lcd.setCursor(0, 1);
+    lcd.print("Version 0.1.1");
+    delay(1000);
+    lcd.clear();
+
+    // countdown
+    for(int i = 3; i > 0; i--) {
         Serial.print("Starting in ");
         Serial.print(i);
         Serial.println(" second(s)!");
@@ -169,13 +170,18 @@ void setup() {
         delay(1000);
     }
     lcd.clear();
-    /*lcd.print("Testing Motor 0");*/
-    /*fire_motor(0, 20);*/
-    /*delay(500);*/
-    /*lcd.clear();*/
-    for(int i = 0; i < 3; i++) {
+
+    for(int i = 0; i < 6; i++) {
         lcd.print("Testing Motor ");
         lcd.print(i);
+        if (i == 9) {
+            lcd.setCursor(1, 1);
+            delay(500);
+            lcd.print("Just kidding!");
+            delay(500);
+            lcd.clear();
+            continue;
+        }
         fire_motor(i, 20);
         delay(1000);
         lcd.clear();
@@ -215,57 +221,7 @@ void loop() {
         receive_joystick_data = "";
     }
     
-    lcd.setCursor(0, 0);
-    lcd.print("X: ");
-    lcd.print(x_velocity);
-    lcd.setCursor(7, 0);
-    lcd.print(" Y: ");
-    lcd.print(y_velocity);
-    lcd.print("    ");
-    lcd.setCursor(0, 1);
-    lcd.print("Z: ");
-    lcd.print(z_velocity);
-    lcd.setCursor(7, 1);
-    lcd.print(" Yaw: ");
-    lcd.print(yaw_velocity);
-    lcd.print("    ");
-    fire_motors(x_velocity, y_velocity, z_velocity, yaw_velocity);
+    print_velocities(x_velocity, y_velocity, z_velocity, yaw_velocity);
+    drive_motors(x_velocity, y_velocity, z_velocity, yaw_velocity);
     delay(10);
 }
-/*void loop() {*/
-    /*
-    // Drive each servo one at a time using setPWM()
-    Serial.println(servonum);
-    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
-    }
-
-    delay(500);
-    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
-    }
-
-    delay(500);
-
-    // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
-    // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior. 
-    for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
-    }
-
-    delay(500);
-    for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-    }
-
-    delay(500);
-
-    servonum++;
-    if (servonum > 1) servonum = 0; // Testing the first 8 servo channels
-    */
-    /*move_y(20);*/
-    /*delay(1000);*/
-    /*move_y(20); // stop the motor*/
-    /*delay(1000);*/
-  
-/*}*/
