5ac38e84efe9bd98834626f39b284e56abf3663b
Clark <91208904+NerdiCat@users.noreply.github.com>
2022-04-25 15:26:18 -0400

    Update new_graber_and_spin_code.ino

--- src/motors/new_graber_and_spin_code/new_graber_and_spin_code.ino
+++ src/motors/new_graber_and_spin_code/new_graber_and_spin_code.ino
@@ -1,58 +1,59 @@
-int rgmp = 3; //right grab motor positive
-int rgmn = 4; //right grab motor negative
-int rsmp = 11; //right spin motor positive
-int rsmn = 10;
+int GrabMotorPos = 3; //right grab motor positive
+int GrabMotorNeg = 4; //right grab motor negative
+int SpinMotorPos = 11; //right spin motor positive
+int SpinMotorNeg = 10;
 int MPWM = 5; //motor power;
-bool in1=false; //grab toggle
-bool in2=false; //spin right
-bool in3=false; //spin left
+bool CloseGrabInput=false; //grab toggle
+bool SpinRightInput=false; //spin right
+bool SpinLeftInput=false; //spin left
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(rgmp, OUTPUT);
-  pinMode(rgmn, OUTPUT);
-  pinMode(rsmp, OUTPUT);
-  pinMode(rsmn, OUTPUT);
+  pinMode(GrabMotorPos, OUTPUT);
+  pinMode(GrabMotorNeg, OUTPUT);
+  pinMode(SpinMotorPos, OUTPUT);
+  pinMode(SpinMotorNeg, OUTPUT);
   pinMode(MPWM, OUTPUT);
   analogWrite(MPWM, 40);
   
 
 }
 void grab(){
-  if (in1){
-    digitalWrite(rgmp, HIGH);
-    digitalWrite(rgmn, LOW);
+  if (CloseGrabInput){
+    digitalWrite(GrabMotorPos, HIGH);
+    digitalWrite(GrabMotorNeg, LOW);
   }
   else {
-    digitalWrite(rgmp, LOW);
-    digitalWrite(rgmn, HIGH);
+    digitalWrite(GrabMotorPos, LOW);
+    digitalWrite(GrabMotorNeg, HIGH);
   }
 }
 void spin(){
-  if (in2){
-    digitalWrite(rsmp, HIGH);
-    digitalWrite(rsmn, LOW);
+  if (SpinRightInput){
+    digitalWrite(SpinMotorPos, HIGH);
+    digitalWrite(SpinMotorNeg, LOW);
   }
-  else if (in3){
-    digitalWrite(rsmp, LOW);
-    digitalWrite(rsmn, HIGH);
+  else if (SpinLeftInput){
+    digitalWrite(SpinMotorPos, LOW);
+    digitalWrite(SpinMotorNeg, HIGH);
   }
   else{
-    digitalWrite(rsmp, LOW);
-    digitalWrite(rsmn, LOW);
+    digitalWrite(SpinMotorPos, LOW);
+    digitalWrite(SpinMotorNeg, LOW);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(5000);
-  in1=true;
+  CloseGrabInput=true;
   grab();
   spin();
   delay(5000);
-  in1=false;
+  CloseGrabInput=false;
   grab();
   spin();
+  
 }
