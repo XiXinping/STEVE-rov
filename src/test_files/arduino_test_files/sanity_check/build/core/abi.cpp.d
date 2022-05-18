4a80842bc06a328427706ff88573cb1eebaee496
XiXinping <about@example.com>
2022-05-18 23:00:47 +0100

    Removed build directories

--- src/test_files/arduino_test_files/sanity_check/build/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/pi/STEVE-rov/src/test_files/arduino_test_files/sanity_check/build/core/abi.cpp.o: \
- /home/pi/.arduino15/packages/arduino/hardware/avr/1.8.5/cores/arduino/abi.cpp
--- src/test_files/arduino_test_files/sanity_check/build/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-/home/pi/STEVE-rov/src/test_files/arduino_test_files/sanity_check/build/core/wiring_pulse.S.o: \
- /home/pi/.arduino15/packages/arduino/hardware/avr/1.8.5/cores/arduino/wiring_pulse.S
