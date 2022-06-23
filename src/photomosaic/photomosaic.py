93e961b56a7306707d9cb67a781e514b4073d604
XiXinping <about@example.com>
2022-06-23 03:18:43 +0100

    Photomosaic stuff (currently not working)

--- src/photomosaic/photomosaic.py
+++ src/photomosaic/photomosaic.py
@@ -1,19 +1,27 @@
-import io
-import PIL
 import pygame.camera
 import pygame.image
+from termcolor import colored
+
+class Camera:
+    def __init__(self):
+        pygame.camera.init()
+        self.camera = pygame.camera.Camera(
+            pygame.camera.list_cameras()[1], (1024, 768))
+        self.camera.start()
+
+    def capture_image(self, image_num):
+        image = self.camera.get_image()
+        pygame.image.save(image, "image{}.jpg".format(image_num))
 
 def main():
-    pygame.camera.init()
-    camera_name = pygame.camera.list_cameras()[1]
-    camera = pygame.camera.Camera(camera_name, 1024, 768)
-    camera.start()
+    print(colored("STEVE's Photomosaic Generator", "blue"))
+    camera = Camera()
 
-    image = camera.get_image()
-    image_string = pygame.image.tostring(image, "RGB", False)
-    pil_image = PIL.Image.frombytes("RGB", image.get_size(), image_string)
-    pil_image.save("image.jpg", "JPEG", quality=95, optimize=True)
+    for i in range(8):
+        input("")
+        camera.capture_image(i)
+
 
 
 if __name__ == "__main__":
-
+    main()
